fe07f68eb0a6e1f425bff58b43cc5bb5c20679ba
luispeixoto <luispeixoto@pq.uenf.br>
2022-07-15 18:43:34 -0300

    chore: configured eslint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,61 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  overrides: [
-    {
-      files: ['*.ts', '*.tsx'],
-      rules: {
-        '@typescript-eslint/no-shadow': ['error'],
-        'no-shadow': 'off',
-        'no-undef': 'off',
-      },
-    },
+  env: {
+    es6: true,
+    jest: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'airbnb',
+    'plugin:prettier/recommended',
+    'prettier',
   ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks', '@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    camelcase: 0,
+    'no-nested-ternary': 0,
+    '@typescript-eslint/camelcase': 'off',
+    'react/no-unstable-nested-components': 0,
+    'react/jsx-one-expression-per-line': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'import/no-duplicates': 'off',
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-empty-interface': 'off',
+    '@typescript-eslint/no-use-before-define': ['error'],
+    'react/jsx-props-no-spreading': 'off',
+    'react/prop-types': 'off',
+    'react/style-prop-object': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/function-component-definition': 'off',
+    'no-unused-expressions': 'off',
+    'react/jsx-filename-extension': [1, {extensions: ['.tsx']}],
+    'import/prefer-default-export': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {},
+    },
+  },
 };
