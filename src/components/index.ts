7942b64f0da8e3c47c3a3a46ca3e7a7ec841b943
luispeixoto <luispeixoto@pq.uenf.br>
2022-07-16 16:40:08 -0300

    refactor: added index components

---
+++ src/components/index.ts
@@ -0,0 +1,21 @@
+import {Bullet} from './Bullet';
+import {Button} from './Button';
+import {CategoryCard} from './CategoryCard';
+import {ClientCard} from './ClientCard';
+import {ImageSlide} from './ImageSlide';
+import {Input} from './Input';
+import {InputPassword} from './InputPassword';
+import {ProductCard} from './ProductCard';
+import {RegisterButton} from './RegisterButton';
+
+export {
+  Bullet,
+  Button,
+  CategoryCard,
+  ClientCard,
+  ImageSlide,
+  Input,
+  InputPassword,
+  ProductCard,
+  RegisterButton,
+};
