a521fcd5fc75cd224b9f4e9d0b8eb40677c6030e
luispeixoto <luispeixoto@pq.uenf.br>
2022-07-16 18:38:11 -0300

    feat: component login screen

---
+++ src/screens/Login/index.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import logo from '../../../assets/images/logo.png';
+import {Button, Input, InputPassword} from '../../components';
+
+import {
+  Container,
+  Image,
+  Title,
+  Subtitle,
+  Form,
+  Footer,
+  FooterText,
+  FooterTextBold,
+  SignupButton,
+} from './styles';
+
+export function Login() {
+  return (
+    <Container>
+      <Image source={logo} />
+      <Title>Bem vindo ao app</Title>
+      <Subtitle>Faça seu login para continuar</Subtitle>
+      <Form>
+        <Input placeholder="E-mail" />
+        <InputPassword placeholder="Senha" />
+        <Button title="Entrar" onPress={() => {}} />
+      </Form>
+      <Footer>
+        <FooterText>Não tem conta? </FooterText>
+        <SignupButton>
+          <FooterTextBold>Cadastre-se</FooterTextBold>
+        </SignupButton>
+      </Footer>
+    </Container>
+  );
+}
