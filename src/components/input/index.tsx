25a8ec507f0a83915a39a4bc5f073145f0875f1c
luispeixoto <luispeixoto@pq.uenf.br>
2022-07-16 12:23:01 -0300

    feat: icon optional

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -4,10 +4,10 @@ import IconType from 'react-native-vector-icons/MaterialIcons';
 import {Container, TextInput, Icon} from './styles';
 
 interface Props extends TextInputProps {
-  icon: React.ComponentProps<typeof IconType>['name'];
+  icon?: React.ComponentProps<typeof IconType>['name'];
 }
 
-export function Input({icon, ...rest}: Props) {
+export function Input({icon = '', ...rest}: Props) {
   const [isFocused, setIsFocused] = useState(false);
 
   function handleInputFocus() {
@@ -20,12 +20,12 @@ export function Input({icon, ...rest}: Props) {
 
   return (
     <Container isFocused={isFocused}>
-      <Icon name={icon} isFocused={isFocused} />
+      {icon && <Icon name={icon} isFocused={isFocused} />}
 
       <TextInput
         isFocused={isFocused}
-        onFocus={handleInputFocus}
-        onBlur={handleInputBlur}
+        onFocus={() => handleInputFocus()}
+        onBlur={() => handleInputBlur()}
         {...rest}
       />
     </Container>
