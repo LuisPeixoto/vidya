e64a0832fab641ff9e749ee7f5e0b1aed3f717cf
luispeixoto <luispeixoto@pq.uenf.br>
2022-07-16 03:37:18 -0300

    chore: added styled-components

---
+++ src/styles/theme.ts
@@ -0,0 +1,16 @@
+export default {
+  colors: {
+    primary: '#5B4486',
+    red: '#FB7181',
+    border: '#F5F5F5',
+    white: '#ffffff',
+  },
+
+  fonts: {
+    light: 'Poppins-Light',
+    regular: 'Poppins-Regular',
+    medium: 'Poppins-Medium',
+    semiBold: 'Poppins-SemiBold',
+    bold: 'Poppins-Bold',
+  },
+};
